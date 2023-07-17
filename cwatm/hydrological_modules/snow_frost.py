a7cc2a1e447edc6dd986f79aad6f38c295eb79e2
Peter Burek <burek@iiasa.ac.at>
2023-07-17 09:42:48 +0200

    snow_frost.py revert changes reverted snow_frost.py changes to my previous version. At a later moment, Peter's changes can be discussed and maybe implement, for now I use my approach.

--- cwatm/hydrological_modules/snow_frost.py
+++ cwatm/hydrological_modules/snow_frost.py
@@ -117,10 +117,6 @@ class snow_frost(object):
 
 
         self.var.ElevationStD = loadmap('ElevationStD')
-
-        #self.var.ElevationMin = loadmap('Elevation')
-        #self.var.ElevationMean = loadmap('Elevation_avg')
-
         # max_frac_snow_redistriution = 0.5
         # max_ELevationStD = 1500
         min_ElevationStD_snow_redistr = 100
@@ -236,11 +232,6 @@ class snow_frost(object):
         self.var.SnowCover = globals.inZero.copy()
         self.var.snow_redistributed_previous = globals.inZero.copy()
 
-        # snow melt potential is collected from up the mountain towards valley
-        snowIceM_surplus = globals.inZero.copy()
-        # snow cover fraction of a gridcell
-        self.var.SnowFraction = globals.inZero.copy()
-
         #get number of elevation zones with forest
         #assume forest is most present at lowest location
         nr_frac_forest = self.var.numberSnowLayers - np.round(self.var.fracVegCover[0] / (1 / self.var.numberSnowLayers)) - 1
@@ -273,7 +264,6 @@ class snow_frost(object):
 
             #if SWE higher than 1m it is assumed that this is unrealistic, therefore it will be melted faster to avoid
             #snow accumulation (similar to implementation in WaterGAP)
-            """
             if np.any(self.var.SnowCoverS[i] >= 1):
                 # the temperature of the lowest elevation zone is used for melting
                 # this will result in an increase in temp for every elevation zone in the grid cell
@@ -283,9 +273,7 @@ class snow_frost(object):
                 SnowMeltHighSWE = (TavgHighSWE - self.var.TempMelt) * SeasSnowMeltCoef * (1 + 0.01 * RainS) * self.var.DtDay
                 SnowMeltS = np.where(self.var.SnowCoverS[i] < 1, SnowMeltNormal, SnowMeltHighSWE)
             else:
-            """
-
-            SnowMeltS = (TavgS - self.var.TempMelt) * SeasSnowMeltCoef * (1 + 0.01 * RainS) * self.var.DtDay
+                SnowMeltS = (TavgS - self.var.TempMelt) * SeasSnowMeltCoef * (1 + 0.01 * RainS) * self.var.DtDay
             SnowMeltS = np.maximum(SnowMeltS, globals.inZero)
 
             # for which layer the ice melt is calculated with the middle temp.
@@ -298,54 +286,34 @@ class snow_frost(object):
             else:
                 IceMeltS = TavgS * self.var.IceMeltCoef * self.var.DtDay * SummerSeason
 
-            # Check snowcover and snowmelt
             IceMeltS = np.maximum(IceMeltS, globals.inZero)
-
-            SnowIceMeltS = np.maximum(np.minimum(SnowMeltS + IceMeltS + snowIceM_surplus, self.var.SnowCoverS[i]), globals.inZero)
-
-            # snowIceM_surplus: each elevation band snow melt potential is collected -> one way to melt additianl snow which might
-            # be colleted in the valley because of snow retribution
-            snowIceM_surplus = np.abs(np.minimum(self.var.SnowCoverS[i] - (SnowMeltS + IceMeltS + snowIceM_surplus),0))
-
+            SnowIceMeltS = np.maximum(np.minimum(SnowMeltS + IceMeltS, self.var.SnowCoverS[i]), globals.inZero)
             IceMeltS = np.maximum(SnowIceMeltS - SnowMeltS, globals.inZero)
             SnowMeltS = np.maximum(SnowIceMeltS - IceMeltS, globals.inZero)
             # check if snow+ice not bigger than snowcover
             self.var.SnowCoverS[i] = self.var.SnowCoverS[i] + SnowS - SnowIceMeltS
 
-            # snow redistribution inspired by Frey and Holzmann (2015) doi:10.5194/hess-19-4517-2015
-            # if snow cover higher than snow holding capacity redistribution
+            #snow redistribution inspired by Frey and Holzmann (2015) doi:10.5194/hess-19-4517-2015
+            #if snow cover higher than snow holding capacity redistribution
             # get the thresholds for the snow based on the snow density and snow depth values in Frey and Holzmann (2015)
-            # capacity of forest 2.5m snow cover, assumed snow density 250kg/m3: 0.25 * 1000 * 2.5 / 1000
+            #capacity of forest 2.5m snow cover, assumed snow density 250kg/m3: 0.25 * 1000 * 2.5 / 1000
             # capacity of other land cover 0.25m snow cover, assumed snow density 250kg/m3: 0.25 * 1000 * 0.25 / 1000
-            # but only for cells with std above 100m
+            #but only for cells with std above 100m
             swe_forest = 0.625
-            swe_other = 0.2
+            swe_other = 0.0625
             # snow capacity depends on whether there is frost cover in the elevation zone
             snowcapacity = np.where(i <= nr_frac_forest, swe_other, swe_forest)
             # where snow cover is higher than capacity, a fraction of snow will be redistributed
-
-            # reduction factor at lowest level no snow_retri, increasing to factor 0.9 at highest level
-            reduction_factor = 1.0 * (1 - (i + 1) / self.var.numberSnowLayers)
-            snow_redistributed = np.where(self.var.SnowCoverS[i] > snowcapacity,
-                    self.var.frac_snow_redistribution * self.var.SnowCoverS[i] * reduction_factor, 0)
-            # the lowest elevation zone cannot redistribute snow -> this is replaced by reduction_factor = 0 in the lowest elevation band
-            #if i == self.var.numberSnowLayers - 1:
-            #    snow_redistributed = globals.inZero.copy()
-
-            snow_redistributed = np.maximum(snow_redistributed, globals.inZero)
+            snow_redistributed = np.where(self.var.SnowCoverS[i] > snowcapacity, self.var.frac_snow_redistribution * self.var.SnowCoverS[i], 0)
+            # the lowest elevation zone cannot redistribute snow
+            if i == self.var.numberSnowLayers - 1:
+                snow_redistributed = globals.inZero
             # the current snow cover will be reduced by the amount of snow that is redistributed
             # the redistributed snow from higher elevation zone will be added
             self.var.SnowCoverS[i] = self.var.SnowCoverS[i] - snow_redistributed + self.var.snow_redistributed_previous
             # redistributed snow will be added to next elevation zone in next loop
-            self.var.snow_redistributed_previous = snow_redistributed.copy()
+            self.var.snow_redistributed_previous = snow_redistributed
 
-            # calculation of snow fraction in each elevation band
-            # =< 0.02 SnowCoverS -> no snow
-            sfrac = np.where(self.var.SnowCoverS[i] > 0.02,0.25,0)
-            sfrac = np.where(self.var.SnowCoverS[i] > 0.05, 0.5,sfrac)
-            sfrac = np.where(self.var.SnowCoverS[i] > 0.10, 1.0, sfrac)
-
-            self.var.SnowFraction += sfrac / self.var.numberSnowLayers
 
             # here outputs are just summed up because equal distribution across elevation zones
             # when glaciers are included the higher elevations should play less of a role
